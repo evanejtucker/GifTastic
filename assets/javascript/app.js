64b2a7f2b8ee4b6aa9344cd901ce1ca4a31b5c1c
Evan Tucker <Evan Tucker>
2017-06-07 10:31:58 -0600

    Version 1.0: Initial File Uploads

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+
+$( document ).ready(function() {
+// Global Variables
+//------------------------------------------------------------------------------
+
+var gifs = ["will ferrell", "family guy", "batman", "pandas", 
+			"bruce lee", "dogeball", "anchorman", "finger guns", 
+			"falling", "table flip" ];
+
+
+
+
+
+// Functions
+//------------------------------------------------------------------------------
+
+
+
+
+
+
+// Main Process
+//------------------------------------------------------------------------------
+
+
+
+
+
+
+});
