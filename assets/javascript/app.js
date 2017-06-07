8f28da7d194b24c6bb15ad08b2f7548da8eea462
Evan Tucker <Evan Tucker>
2017-06-07 11:07:17 -0600

    version 1.2: added button array functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
 $( document ).ready(function() {
 // Global Variables
 //------------------------------------------------------------------------------
+var userInput;
 
 var gifs = ["will ferrell", "family guy", "batman", "pandas", 
 			"bruce lee", "dogeball", "anchorman", "finger guns", 
@@ -14,7 +15,21 @@ var gifs = ["will ferrell", "family guy", "batman", "pandas",
 // Functions
 //------------------------------------------------------------------------------
 
+gifButtons = function() {
+	for (i=0; i<gifs.length; i++) {
+        // console.log(letters[i]);
+        var gifBtn = $("<button>");
+        gifBtn.addClass("btn btn-default gifButton");
+        // letterBtn.attr("data-letter", letters[i]);
+        gifBtn.text(gifs[i]);
+        $("#gifButtonLibrary").append(gifBtn);
+      }
+}
 
+newGifButton = function(userInput) {
+	userInput = $("#exampleInputText1").text();
+	alert(userinput);
+}
 
 
 
@@ -22,8 +37,13 @@ var gifs = ["will ferrell", "family guy", "batman", "pandas",
 // Main Process
 //------------------------------------------------------------------------------
 
+gifButtons();
 
-
+// this click function will put the user inout into the gif array
+$("#gifSubmit").on("click", function() {
+        console.log("test");
+        newGifButton();
+      });
 
 
 
