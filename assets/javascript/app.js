a47d94ddbdc2ef512444d594c5d1a4e508752d42
Evan Tucker <Evan Tucker>
2017-06-09 09:28:59 -0600

    version 3.0: fixed button array functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,70 +2,49 @@
 $( document ).ready(function() {
 // Global Variables
 //------------------------------------------------------------------------------
-var userInput;
 
 var gifs = ["will ferrell", "family guy", "batman", "pandas", 
 			"bruce lee", "dogeball", "anchorman", "finger guns", 
 			"falling", "table flip", ];
 
-var selectedGif = "";
 
 
-
+ 
 // Functions
 //------------------------------------------------------------------------------
 
 // adds buttons for gifs array
 gifButtons = function() {
+
+	$("#gifButtonLibrary").empty();
+
 	for (i=0; i<gifs.length; i++) {
-        // console.log(letters[i]);
         var gifBtn = $("<button>");
-        gifBtn.addClass("btn btn-default gifButton");
-        // letterBtn.attr("data-letter", letters[i]);
+        gifBtn.addClass("btn btn-success gifButton");
+        gifBtn.attr("giphy", gifs[i]);
         gifBtn.text(gifs[i]);
         $("#gifButtonLibrary").append(gifBtn);
       }
-}
 
-newGifButton = function() {
-	userInput = $("#exampleInputText1").val();
-	console.log(userInput);
-	String(userInput);
-	gifs.push(userInput);
-	// gifButtons();
 }
-
-gifSet = function(selectedGif) {
-	selectedGif = gifs[i];
-	alert(selectedGif);
-}
-
 
 
 
 // Main Process
 //------------------------------------------------------------------------------
 
+//this click function will put the user inout into the gif array
+$("#gifSubmit").on("click", function(event) {
+
+        event.preventDefault();
+
+        var newButton = $("#newGifButtons").val();
+        gifs.push(newButton);
+        gifButtons();
+        $("#inputForm").children("input").val("");
+      });
+
 gifButtons();
-
-
-// this click function will put the user inout into the gif array
-// $("#gifSubmit").on("click", function() {
-//         newGifButton();
-//         gifButtons();
-//       });
-
-$("#gifSubmit").click(function() {
-		// gifButtons();
-        newGifButton(); 
-      });
-
-
-$("#gifButtonLibrary").on("click", function(event) {
-		gifSet(event.curentTarget);
-      });
-
-console.log(gifs);
 
 
 });
