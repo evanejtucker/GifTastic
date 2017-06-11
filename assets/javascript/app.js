f11b93a91da091d2300956c0c3a566995ff519ba
Ryan Hansen <rphansen91@gmail.com>
2017-06-11 14:04:58 -0700

    Gif display scope, media query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
 // Global Variables
 //------------------------------------------------------------------------------
 
-var gifs = ["will ferrell", "family guy", "batman", "pandas", 
-			"bruce lee", "dogeball", "anchorman", "finger guns", 
+var gifs = ["will ferrell", "family guy", "batman", "pandas",
+			"bruce lee", "dogeball", "anchorman", "finger guns",
 			"falling", "table flip", "facepalm", "highfive", "shrek",
 			"wink", "crying", "slow motion", "hedgehog", "canada",
 			"grand theft auto", "fail", "funny", "laughing", "samuel l jackson",
@@ -12,7 +12,7 @@ var gifs = ["will ferrell", "family guy", "batman", "pandas",
 
 
 
- 
+
 // Functions
 //------------------------------------------------------------------------------
 
@@ -22,45 +22,32 @@ gifButtons = function() {
 	$("#gifButtonLibrary").empty();
 
 	for (i=0; i<gifs.length; i++) {
-        var gifBtn = $("<button>");
-        gifBtn.addClass("btn btn-success gifButton");
-        gifBtn.attr("giphy", gifs[i]);
-        gifBtn.text(gifs[i]);
-        $("#gifButtonLibrary").append(gifBtn );
-      }
+    var gifBtn = GifButton(gifs[i]);
+    $("#gifButtonLibrary").append(gifBtn);
+  }
 }
 
 // function to display gifs using giphy API
 displayGifs = function () {
-
+  $("#gifDisplay").html('Loading...');
 	var gifName = $(this).attr("giphy");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+gifName+"&api_key=dc6zaTOxFJmzC&limit=10";
-	
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q="+gifName+"&api_key=dc6zaTOxFJmzC&limit=10";
 
-	$.ajax({
-		url: queryURL,
-		method: "GET",
+  setTimeout(function () {
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      $("#gifDisplay").html('');
+      var results = response.data;
 
-	}).done(function(response) {
-		var results = response.data;
+      for (i=0; i<results.length; i++) {
+        var gifImage = GifDisplay(response.data[i]);
+        $("#gifDisplay").prepend(gifImage);
+      }
 
-		for (i=0; i<results.length; i++) {
-			var playGif = response.data[i].images.fixed_height.url;
-			var pauseGif = response.data[i].images.fixed_height_still.url;
-			var gifRating = response.data[i].rating;
-
-			var rating = $("<p>").text("rating: " + gifRating);
-			var gifImage = $("<img class='gif'></img>");
-
-			gifImage.addClass("gifImage");
-			gifImage.attr("src", pauseGif);
-			$("#gifDisplay").prepend(gifImage);
-			$(".gif").prepend(rating);
-
-		}
-		
-	});
-
+    });
+  }, 2000);
 }
 
 
@@ -78,20 +65,40 @@ $("#gifSubmit").on("click", function(event) {
         $("#inputForm").children("input").val("");
       });
 
-// click function for gif buttons, displays gifs
-$(document).on("click", ".gifButton", displayGifs);
 
-// click function to play/pause gifs
-$(document).on("click", ".gif", function(){
-	console.log(this);
-	state = $(this).attr("src");
-	if( state === pauseGif) {
-		alert("pasue");
-	} else {
-		alert("not paused");
-	}
+function GifButton (btn) {
+  var gifBtn = $("<button>");
+  gifBtn.addClass("btn btn-success gifButton");
+  gifBtn.attr("giphy", btn);
+  gifBtn.text(btn);
+  gifBtn.on('click', displayGifs);
+  return gifBtn;
+}
 
-});
+function GifDisplay (gif) {
+  var playGif = gif.images.fixed_height.url;
+  var pauseGif = gif.images.fixed_height_still.url;
+  var gifRating = gif.rating;
+  var playstate = false;
+
+  var gifElement = $("<div class='gif'></div>")
+  var rating = $("<p>rating: " + gifRating+"</p>");
+  var gifImage = $("<img></img>");
+  gifElement.append(rating);
+  gifElement.append(gifImage);
+  gifImage.addClass("gifImage");
+  gifImage.attr("src", pauseGif);
+  gifImage.on('click', function () {
+    if (!playstate) {
+      gifImage.attr("src", playGif);
+    } else {
+      gifImage.attr("src", pauseGif);
+    }
+    playstate = !playstate
+  })
+
+  return gifElement
+}
 
 
 gifButtons();
